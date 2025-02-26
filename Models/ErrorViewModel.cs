f8ae52fc0827764aaae688972ad61e9bb08d8175
julie-min <jiyun3664@gmail.com>
2025-02-27 00:12:05 +1300

    초기 커밋.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SmartPOSWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
